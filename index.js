e2f83446992c73d52a7f36b3bacea9bb894a930d
afk0323 <jwv0323@naver.com>
2021-11-25 03:33:29 +0900

    수정

---
+++ index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){ 
+    var currentPosition = parseInt($(".quickmenu").css("top")); 
+    $(window).scroll(function() { var position = $(window).scrollTop(); 
+        $(".quickmenu").stop().animate({"top":position+currentPosition+"px"},1000); 
+    }); 
+});

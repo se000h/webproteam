9c39c5a7e8665075c26d4d46a0528a6247892749
ddingmin <ddingminyt@gmail.com>
2021-12-01 11:55:30 +0900

    consulting

---
+++ remind_consulting/delete.php
@@ -0,0 +1,33 @@
+<?php
+    include_once "./inc/_head.php";
+
+    $id = $_GET["id"];
+    $page = $_GET['page'];
+?>
+<div class="container">
+    <div class="content row">
+
+        <?php include_once "./inc/_nav.php"; ?>
+
+        <section class="col-10">
+
+            <form action="deleteDo.php" method="POST" id="passwd_form">
+                <div class="pass_title">
+                    삭제를 위해 패스워드를 입력해주세요.
+                </div>
+
+                <div class="pass_input">
+                    <input type="password" name="del_passwd" required />
+                    <input type="hidden" name="id" value="<?=$id?>" />
+                    <input type="hidden" name="page" value="<?=$page?>" />
+                </div>
+
+                <div>
+                    <button type="submit" class="btn btn-primary">확인</button>
+                    <button type="button" class="btn btn-secondary" onClick="history.back(-1);">뒤로가기</button>
+                </div>
+
+            </form>
+        </section>
+    </div>
+</div>
---
+++ remind_consulting/password.php
@@ -0,0 +1,36 @@
+<?php
+    include_once "./inc/_head.php";
+
+    $id = $_GET["id"];
+    $page = $_GET['page'];
+    $mode = $_GET['mode'];
+?>
+<div class="container">
+    <div class="content row">
+
+        <?php include_once "./inc/_nav.php"; ?>
+
+        <section class="col-10">
+
+            <form action="passwordDo.php" method="POST" id="passwd_form">
+                <div class="pass_title">
+                    패스워드를 입력해주세요.
+                </div>
+
+                <div class="pass_input">
+                    <input type="password" name="bb_passwd" required />
+                    <input type="hidden" name="id" value="<?=$id?>" />
+                    <input type="hidden" name="page" value="<?=$page?>" />
+                    <input type="hidden" name="mode" value="<?=$mode?>" />
+                </div>
+
+                <div>
+                    <button type="submit" class="btn btn-primary">확인</button>
+                    <button type="button" class="btn btn-secondary" onClick="history.back(-1);">뒤로가기</button>
+                    <a href="./" class="btn btn-secondary">목록</a>
+                </div>
+
+            </form>
+        </section>
+    </div>
+</div>

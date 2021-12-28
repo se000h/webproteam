b78434f9d1535be9f988bd491423b0e8ddcc5ae3
afk0323 <jwv0323@naver.com>
2021-12-29 01:32:41 +0900

    수정
    수정

--- remind_order/password.php
+++ remind_order/password.php
@@ -5,7 +5,7 @@
     $page = $_GET['page'];
     $mode = $_GET['mode'];
 ?>
-<div class="container">
+<div class="container text-center">
     <div class="content row">
 
         <?php include_once "./inc/_nav.php"; ?>
